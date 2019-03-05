e3907a9dcfe8b238f0adbddd6f795ccf3d046289
Michael D Rozario <45930782+jdrozar3@users.noreply.github.com>
2019-03-05 00:07:29 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,19 @@
-$('.quote').ready(() => {
+$(document).ready(() => {
     $('.hide').fadeIn(2000).removeClass();
+
+
+
+
+    $('#lab').on("click", () => {
+        $('body').css('background-image', 'url("./Imgs/art2.png")');
+
+
+
+    });
+    $('#lab').on("dblclick", () => {
+        $('body').css('background-image', 'url("./Imgs/artnew.png")');
+
+
+
+    });
 });
